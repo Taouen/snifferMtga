ae583118e979e29e7d89a01d9d7eacfd6ae477c5
Tanner Wiltshire <tanner.wiltshire@gmail.com>
2020-11-05 22:39:17 -0600

    changing collection to rares and mythics

--- src/App2.js
+++ src/App2.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import CardListItem from './components/CardListItem';
 
+/* To DO
+ 
+  - import set symbol
+  - enter quantity (min 0, max 4)
+  - enter packs owned
+  -  
+*/
+
 class App2 extends React.Component {
   state = {
     currentSet: 'znr',
-    cardsList: [],
+    mythics: [],
+    rares: [],
     raritySelector: 'rare',
   };
 
   getSetData = async (set) => {
     const cardSet = await fetch(`https://api.scryfall.com/sets/${set}`);
-    const cardsList = [];
+    const rares = [];
+    const mythics = [];
 
     cardSet.json().then((data) => {
       const getCards = async (uri) => {
@@ -19,25 +29,42 @@ class App2 extends React.Component {
             if (data.has_more) {
               data.data.map((item) => {
                 if (
-                  (item.rarity === 'rare' || item.rarity === 'mythic') &&
-                  !cardsList.some((element) => element.name === item.name)
-                )
-                  cardsList.push(item);
+                  item.rarity === 'rare' &&
+                  !rares.some((element) => element.name === item.name)
+                ) {
+                  item.collected = 0;
+                  rares.push(item);
+                } else if (
+                  item.rarity === 'mythic' &&
+                  !mythics.some((element) => element.name === item.name)
+                ) {
+                  item.collected = 2;
+                  mythics.push(item);
+                }
               });
               getCards(data.next_page);
             } else {
               data.data.map((item) => {
                 if (
-                  (item.rarity === 'rare' || item.rarity === 'mythic') &&
-                  !cardsList.some((element) => element.name === item.name)
-                )
-                  cardsList.push(item);
-                localStorage.setItem(set, JSON.stringify(cardsList));
+                  item.rarity === 'rare' &&
+                  !rares.some((element) => element.name === item.name)
+                ) {
+                  item.collected = 0;
+                  rares.push(item);
+                } else if (
+                  item.rarity === 'mythic' &&
+                  !mythics.some((element) => element.name === item.name)
+                ) {
+                  item.collected = 0;
+                  mythics.push(item);
+                }
               });
             }
+            localStorage.setItem(`${set}Rares`, JSON.stringify(rares));
+            localStorage.setItem(`${set}Mythics`, JSON.stringify(mythics));
           });
         });
-        this.setState({ cardsList });
+        this.setState({ rares, mythics });
       };
 
       getCards(data.search_uri);
@@ -45,9 +72,10 @@ class App2 extends React.Component {
   };
 
   getLocalSetData = (set) => {
-    if (localStorage.getItem(set)) {
-      const cardsList = JSON.parse(localStorage.getItem(set));
-      this.setState({ cardsList });
+    if (localStorage.getItem(`${set}Rares`)) {
+      const rares = JSON.parse(localStorage.getItem(`${set}Rares`));
+      const mythics = JSON.parse(localStorage.getItem(`${set}Mythics`));
+      this.setState({ rares, mythics });
     } else {
       this.getSetData(set);
     }
@@ -59,11 +87,78 @@ class App2 extends React.Component {
     this.setState({ currentSet });
   };
 
+  handleRarityChange = (event) => {
+    const raritySelector = event.target.value;
+    this.setState({ raritySelector });
+  };
+
+  // Working on writing two formulas, one to select the card to update, and one to update the collected value for the card, then call them both within a single function to pass to the component.
+
+  flagForUpdate = (name) => {
+    const { rares, mythics } = this.state;
+  };
+
+  updateCollected = (e) => {
+    const { rares, mythics } = this.state;
+    console.log(e);
+  };
+
   componentDidMount = () => {
     this.getLocalSetData(this.state.currentSet);
   };
 
   render() {
+    let list;
+    if (this.state.raritySelector === 'rare') {
+      list = (
+        <ul>
+          {this.state.rares.map((item) => {
+            const { collected, id, name } = item;
+            let color;
+            if (item.card_faces) {
+              color = item.card_faces[0].colors;
+            } else {
+              color = item.colors;
+            }
+
+            return (
+              <CardListItem
+                collected={collected}
+                key={id}
+                name={name}
+                colors={color}
+                updateCollected={this.updateCollected}
+              />
+            );
+          })}
+        </ul>
+      );
+    } else if (this.state.raritySelector === 'mythic') {
+      list = (
+        <ul>
+          {this.state.mythics.map((item) => {
+            const { collected, id, name } = item;
+            let color;
+            if (item.card_faces) {
+              color = item.card_faces[0].colors;
+            } else {
+              color = item.colors;
+            }
+
+            return (
+              <CardListItem
+                collected={collected}
+                key={id}
+                name={name}
+                colors={color}
+                updateCollected={this.updateCollected}
+              />
+            );
+          })}
+        </ul>
+      );
+    }
+
     return (
       <div className="App">
         <header className="App-header">MTGA Collection Tracker</header>
@@ -73,19 +168,20 @@ class App2 extends React.Component {
           onChange={this.handleSetChange}
         >
           <option value="znr">Zendikar Rising</option>
-          <option value="m21" defaultValue>
-            Core Set 2021
-          </option>
+          <option value="m21">Core Set 2021</option>
           <option value="iko">Ikoria</option>
           <option value="thb">Theros Beyond Death</option>
           <option value="eld">Throne of Eldraine</option>
         </select>
-        {this.state.cardsList.map((item) => {
-          const { id, name, rarity } = item;
-          if (rarity === this.state.raritySelector) {
-            return <CardListItem key={id} id={id} name={name} />;
-          }
-        })}
+        <select
+          id="rarity"
+          value={this.state.raritySelector}
+          onChange={this.handleRarityChange}
+        >
+          <option value="rare">Rare</option>
+          <option value="mythic">Mythic Rare</option>
+        </select>
+        {list}
       </div>
     );
   }
--- src/components/CardListItem.js
+++ src/components/CardListItem.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import NumericInput from 'react-numeric-input';
 
 class CardListItem extends React.Component {
   render = () => {
-    const { name } = this.props;
-    return <p>{name}</p>;
+    const { collected, colors, name, updateCollected } = this.props;
+
+    return (
+      <li className={colors}>
+        {name}
+        <NumericInput
+          min={0}
+          max={4}
+          value={collected}
+          onChange={updateCollected}
+        />
+        {/* <select
+          id={name}
+          value={this.state.collected}
+          // when passing name as an argument, event becomes name instead of the select object
+          onChange={updateCollected}
+        >
+          <option value="0">0</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+        </select> */}
+      </li>
+    );
   };
 }
 
