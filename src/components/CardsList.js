c789de47c2464d5cfed068d7f354f6d8ba3d12a5
Quinn Keast <qkeast@sourcegraph.com>
2021-04-14 18:11:02 +0200

    Fix render issue, update design a bit

--- src/components/CardsList.js
+++ src/components/CardsList.js
@@ -2,21 +2,35 @@ import canBeCast from './castingChecker';
 
 export default function CardsList({ cards, mana, totalMana, setControls }) {
   const filteredCards = [];
+  
   cards.forEach((card) => {
     if (canBeCast(card, mana, totalMana, setControls)) {
       filteredCards.push(card);
     }
   });
-
+  
+  console.log(filteredCards);
+  
+  // TODO – improve this function so that it fetches the correct face
+  const getCardImage = (card) => {
+    if (card.image_uris) return card.image_uris.normal;
+    if (card.card_faces) {
+      if (card.card_faces[0].image_uris) return card.card_faces[0].image_uris.normal;
+    } else {
+      return null;
+    }
+  }
+  
   return (
     <ul className="flex flex-wrap justify-center">
       {filteredCards.map((card) => {
-        const { mana_cost, image_uris, id, name } = card;
+        const { mana_cost, image_uris, card_faces, id, name } = card;
+        
         return (
           <li key={id}>
             <img
               className="m-1 rounded-lg w-48 h-64"
-              src={image_uris.normal}
+              src={getCardImage(card)}
               alt={`${name}, ${mana_cost}`}
             />
           </li>
