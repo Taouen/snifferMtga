f7f93e089b8df2db28ef589b1e0b6d6534ec2a97
Tanner Wiltshire <tanner.wiltshire@gmail.com>
2021-04-29 08:18:25 -0500

    moving changes to branch develop

--- src/App.js
+++ src/App.js
@@ -86,6 +86,7 @@ class App extends React.Component {
       };
 
       if (data.parent_set_code) {
+        getCards(data.search_uri);
         const getParentSetCards = async () => {
           const parentSet = await fetch(
             `https://api.scryfall.com/sets/${data.parent_set_code}`
@@ -94,7 +95,6 @@ class App extends React.Component {
             getCards(data.search_uri);
           });
         };
-        getCards(data.search_uri);
         getParentSetCards();
       } else {
         getCards(data.search_uri);
