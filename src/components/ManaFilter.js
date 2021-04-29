9866fa6488dfc0c170f41c3a3b6d439dd83986a4
Tanner Wiltshire <tanner.wiltshire@gmail.com>
2021-04-29 08:20:50 -0500

    pulled changes from remote

--- src/components/ManaFilter.js
+++ src/components/ManaFilter.js
@@ -32,14 +32,23 @@ export default function ColorFilter(props) {
           return (
             <div key={index} className="flex flex-col items-center">
               <div className="flex flex-col justify-around">
-                <button onClick={() => handleManaChange(color, 1)} className="flex flex-col items-center text-2xl px-4">
-                  <span className={mana[color] > 0 ? 'text-black' : 'text-gray-200'}>{mana[color]}</span>
+                <button
+                  onClick={() => handleManaChange(color, 1)}
+                  className="flex flex-col items-center text-2xl px-4"
+                >
+                  <span
+                    className={mana[color] > 0 ? 'text-black' : 'text-gray-200'}
+                  >
+                    {mana[color]}
+                  </span>
                   <div className="w-10 block">{manaSymbol(color)}</div>
                 </button>
               </div>
               <div className="flex flex-row mt-4">
                 <button
-                  className={`${mana[color] > 0 ? 'text-gray-800' : 'text-gray-300'} text-xs px-1.5 py-0.5 rounded bg-gray-100`}
+                  className={`${
+                    mana[color] > 0 ? 'text-gray-800' : 'text-gray-300'
+                  } text-xs px-1.5 py-0.5 rounded bg-gray-100`}
                   type="button"
                   onClick={() => handleManaChange(color, -1)}
                   disabled={mana[color] > 0 ? '' : 'disabled'}
