1109cdbce14e4daf71d19b4930fea5fdac2245d5
Tanner Wiltshire <tanner.wiltshire@gmail.com>
2023-09-11 12:41:23 -0500

    Update current set to Wilds of Eldraine

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import * as scryfall from 'scryfall-client';
 class App extends React.Component {
   state = {
     cards: [],
-    currentSet: 'mul',
+    currentSet: 'wot',
     error: false,
     loading: true,
     mana: {
